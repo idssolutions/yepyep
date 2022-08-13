b0a45b40753e51d1663a4b2f6eadc5793a4654c3
yepyeplabs <89113968+yepyeplabs@users.noreply.github.com>
2022-08-13 11:45:26 -0700

    Add some spacing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return (<div className="container mx-auto">
+  return (<div className="container mx-auto w-2/3 mt-44">
     <Component {...pageProps} />
   </div>)
 }
